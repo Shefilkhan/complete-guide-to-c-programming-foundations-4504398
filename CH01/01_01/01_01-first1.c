1c5b692b3e423b1c11b73dda9128d6b660016be0
Shefilkhan <spathan8633@conestogac.on.ca>
2025-06-30 02:40:53 -0400

    Initial Commit: My first code

--- CH01/01_01/01_01-first1.c
+++ CH01/01_01/01_01-first1.c
@@ -1,2 +1,22 @@
+/*
+ FILE			: 01_01-first.c
+ PROGRAMMER		: Shefilkhan Pathan
+ FIRST VERSION	: 2025-06-30
+ DESCRIPTION	: This simple C program prints "Hello, world!" to the console
+                  using the printf() function from the standard I/O library.
+*/
+#include<stdio.h>
 
+/*
+ * FUNCTION      : main
+ * DESCRIPTION   : Entry point for the program.
+ * PARAMETERS    : void
+ * RETURNS       : int - Returns 0 when the program exits successfully.
+ */
+int main(void) {
 
+    printf("Hello, World!\n");
+    printf("I am writting C programming code\n");
+
+    return 0;
+}
