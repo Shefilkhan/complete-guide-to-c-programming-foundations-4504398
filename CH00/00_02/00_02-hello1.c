7a081b4c7254b08d7344b96bd9b28ab919c1e1d7
Shefilkhan <spathan8633@conestogac.on.ca>
2025-06-30 02:22:33 -0400

    Mention comments according to SET Coding Standards.

--- CH00/00_02/00_02-hello1.c
+++ CH00/00_02/00_02-hello1.c
@@ -1,8 +1,23 @@
+/*
+ * FILE          : 00_02-hello1.c
+ * PROGRAMMER    : Shefilkhan Pathan
+ * DATE          : 2025-06-30
+ * DESCRIPTION   : This simple C program prints "Hello, world!" to the console
+ *                 using the printf() function from the standard I/O library.
+ */
+
 #include <stdio.h>
 
-int main()
+ /*
+  * FUNCTION      : main
+  * DESCRIPTION   : Entry point for the program.
+  *                 This function prints a "Hello, world!" message.
+  * PARAMETERS    : void
+  * RETURNS       : int - Returns 0 when the program exits successfully.
+  */
+int main(void)
 {
-	printf("Hello, world!");
+	printf("Hello, world!"); // Print the greeting message to the console
 
 	return 0;
 }
